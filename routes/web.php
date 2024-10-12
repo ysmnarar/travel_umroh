77b223ae023b0978c8c3461792bd48153a12efdf
Rara <yasminathiyya@gmail.com>
2024-10-12 10:46:15 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use App\Http\Controllers\JamaahController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BaseController@app');
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
